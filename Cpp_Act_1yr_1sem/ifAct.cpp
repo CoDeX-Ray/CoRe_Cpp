475f17e9d659d073632606d9dab4d347a49c52cf
Raymund <142377798+CoDeX-Ray@users.noreply.github.com>
2024-03-12 18:15:37 +0800

    Create ifAct.cpp

---
+++ Cpp_Act_1yr_1sem/ifAct.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Declaration
+  int grade = 90;
+
+  // Condition
+  if (grade >= 90)
+  {
+    cout << "Passed";
+  }
+  else
+  {
+    cout << "Failed";
+  }
+
+  return 0;
+}
